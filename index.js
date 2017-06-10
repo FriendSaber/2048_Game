8b4ef634dfeaa3c1ffc22de5b4e807a68b5a9cb7
NULL <NULL>
2017-06-10 16:20:15 +0800

    Changed:事件监听

--- index.js
+++ index.js
@@ -18,38 +18,54 @@ var grid =[];
 for(var i=0; i<4;i++){
     grid[i] = [0,0,0,0];
 }
-
+var rowIndex=generateRandom();
+var columnIndex=generateRandom();
+grid[rowIndex][columnIndex]=2;
 for(var i=0;i<4;i++){
     for(var j=0;j<4;j++){
-        var graphics=new PIXI.Graphics();
-        graphics.beginFill(0x3A97FF, 1);
-        graphics.drawRoundedRect(app.renderer.width/8+160*j, app.renderer.height/7*3+160*i,156, 156);
-        app.stage.addChild(graphics);
+        drawCell(i,j);
+        //var graphics=new PIXI.Graphics();
+        //graphics.beginFill(0x3290FA, 1);
+        //graphics.drawRoundedRect(app.renderer.width/8+160*j, app.renderer.height/7*3+160*i,156, 156);
+        //app.stage.addChild(graphics);
     }
 }
 
 function generateRandom(){
     return Math.floor(Math.random()*4)
 }
-var x=generateRandom();
-var y=generateRandom();
 
-for(var i=0;i<4;i++){
-    for(var j=0;j<4;j++){
-        var graphics=new PIXI.Graphics();
-        graphics.beginFill(0xEFF6FE, 1);
-        graphics.drawRoundedRect(app.renderer.width/8+160*y, app.renderer.height/7*3+160*x,154, 154);
-        app.stage.addChild(graphics);
+
+function drawCell(rowIndex,columnIndex) {
+
+    var color=0x99D9EA;
+    if(grid[rowIndex][columnIndex]===2){
+        color=0x99D9bc;
     }
-}
+    var graphics = new PIXI.Graphics();
+    graphics.beginFill(color, 1);
+    graphics.drawRoundedRect(app.renderer.width / 8 + 160 * columnIndex, app.renderer.height / 7 * 3 + 160 * rowIndex, 154, 154);
+    app.stage.addChild(graphics);
 
-var number = new PIXI.Text('2',{
-    fontSize:150
+
+    if(grid[rowIndex][columnIndex]!= 0 ){
+        var number = new PIXI.Text(grid[rowIndex][columnIndex], {
+            fontSize: 150
+        });
+
+        number.anchor.set(0.5);
+        number.x = 156 / 2 + app.renderer.width / 8 + 160 * columnIndex;//230;
+        number.y = 156 / 2 + app.renderer.height / 7 * 3 + 160 * rowIndex;//90;
+
+        app.stage.addChild(number);
+    }
+
+};
+
+document.addEventListener('keydown',function(event){
+    if(event.code== "ArrowUp"){
+        console.log(event)
+    }
 });
 
-number.anchor.set(0.5);
-number.x = 156/2+app.renderer.width/8+160*y;//230;
-number.y = 156/2+app.renderer.height/7*3+160*x;//90;
-
-app.stage.addChild(number);
 
