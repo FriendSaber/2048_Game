94eda00dd45f91a223fb4fe0dcaaeafee201d480
NULL <NULL>
2017-06-10 17:17:47 +0800

    Changed:完成合并

--- index.js
+++ index.js
@@ -16,20 +16,23 @@ app.stage.addChild(basicText);
 
 var grid =[];
 for(var i=0; i<4;i++){
-    grid[i] = [0,0,0,0];
+    grid[i] = [2,2,0,0];
 }
-var rowIndex=generateRandom();
-var columnIndex=generateRandom();
-grid[rowIndex][columnIndex]=2;
-for(var i=0;i<4;i++){
-    for(var j=0;j<4;j++){
-        drawCell(i,j);
-        //var graphics=new PIXI.Graphics();
-        //graphics.beginFill(0x3290FA, 1);
-        //graphics.drawRoundedRect(app.renderer.width/8+160*j, app.renderer.height/7*3+160*i,156, 156);
-        //app.stage.addChild(graphics);
+
+
+
+var FlashUI = function () {
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            drawCell(i, j);
+            //var graphics=new PIXI.Graphics();
+            //graphics.beginFill(0x3290FA, 1);
+            //graphics.drawRoundedRect(app.renderer.width/8+160*j, app.renderer.height/7*3+160*i,156, 156);
+            //app.stage.addChild(graphics);
+        }
     }
-}
+};
+FlashUI();
 
 function generateRandom(){
     return Math.floor(Math.random()*4)
@@ -43,10 +46,9 @@ function drawCell(rowIndex,columnIndex) {
         color=0x99D9bc;
     }
     var graphics = new PIXI.Graphics();
-    graphics.beginFill(color, 1);
+    graphics.beginFill(getColorByNumber(grid[rowIndex][columnIndex]), 1);
     graphics.drawRoundedRect(app.renderer.width / 8 + 160 * columnIndex, app.renderer.height / 7 * 3 + 160 * rowIndex, 154, 154);
     app.stage.addChild(graphics);
-
 
     if(grid[rowIndex][columnIndex]!= 0 ){
         var number = new PIXI.Text(grid[rowIndex][columnIndex], {
@@ -62,10 +64,57 @@ function drawCell(rowIndex,columnIndex) {
 
 };
 
+var rowIndex=generateRandom();
+var columnIndex=generateRandom();
+grid[rowIndex][columnIndex]=2;
+
+drawCell(rowIndex, columnIndex);
+
+function getColorByNumber(number) {
+    var colorValue = {
+        0: 0x00FF00,
+        2: 0xFF0000,
+        4: 0x0000FF
+    };
+
+    return colorValue[number];
+}
+
 document.addEventListener('keydown',function(event){
-    if(event.code== "ArrowUp"){
+    if(event.code== "ArrowRight"){
         console.log(event)
+        moveCellToRight();
+        FlashUI();
     }
 });
 
+function moveCellToRight() {
+    for (var rowIndex = 0; rowIndex < 4; rowIndex++) {
+        for (var columnIndex = 2; columnIndex >= 0; columnIndex--) {
+            if (grid[rowIndex][columnIndex] === 0) continue;
+
+            var theEmptyCellIndex = findTheFirstRightCell(rowIndex, columnIndex);
+            if (theEmptyCellIndex !== -1) {
+                grid[rowIndex][theEmptyCellIndex] = grid[rowIndex][columnIndex];
+                grid[rowIndex][columnIndex] = 0;
+
+                if (grid[rowIndex][theEmptyCellIndex] === grid[rowIndex][theEmptyCellIndex + 1]) {
+                    grid[rowIndex][theEmptyCellIndex+ 1] += grid[rowIndex][theEmptyCellIndex];
+                    grid[rowIndex][theEmptyCellIndex] = 0;
+                }
+            }
+
+        }
+    }
+}
+
+
+function findTheFirstRightCell(rowIndex, columnIndex) {
+    for (var i = 3; i > columnIndex; i--) {
+        if (grid[rowIndex][i] === 0) {
+            return i;
+        }
+    }
+    return -1;
+}
 
