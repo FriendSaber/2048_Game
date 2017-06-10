f58e1de4fa8db2dd02d2287a3dd901dfcd972a0f
NULL <NULL>
2017-06-10 14:50:55 +0800

    Changed:随机数产生

--- index.js
+++ index.js
@@ -22,8 +22,34 @@ for(var i=0; i<4;i++){
 for(var i=0;i<4;i++){
     for(var j=0;j<4;j++){
         var graphics=new PIXI.Graphics();
-        graphics.beginFill(0xFFCD72, 1);
-        graphics.drawRect(app.renderer.width/8+160*j, app.renderer.height/8*3+160*i,156, 156);
+        graphics.beginFill(0x3A97FF, 1);
+        graphics.drawRoundedRect(app.renderer.width/8+160*j, app.renderer.height/7*3+160*i,156, 156);
         app.stage.addChild(graphics);
     }
 }
+
+function generateRandom(){
+    return Math.floor(Math.random()*4)
+}
+var x=generateRandom();
+var y=generateRandom();
+
+for(var i=0;i<4;i++){
+    for(var j=0;j<4;j++){
+        var graphics=new PIXI.Graphics();
+        graphics.beginFill(0xEFF6FE, 1);
+        graphics.drawRoundedRect(app.renderer.width/8+160*y, app.renderer.height/7*3+160*x,154, 154);
+        app.stage.addChild(graphics);
+    }
+}
+
+var number = new PIXI.Text('2',{
+    fontSize:150
+});
+
+number.anchor.set(0.5);
+number.x = 156/2+app.renderer.width/8+160*y;//230;
+number.y = 156/2+app.renderer.height/7*3+160*x;//90;
+
+app.stage.addChild(number);
+
