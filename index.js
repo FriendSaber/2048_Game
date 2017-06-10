18222767926bdc13ba29c107ffc5c8d74c68a07c
NULL <NULL>
2017-06-10 14:08:48 +0800

    Changed:grid完成2

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 /**
  * Created by 荼蘼 on 2017-06-10.
  */
-var app = new PIXI.Application(800, 600, {backgroundColor: 0x1099bb});
+var app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroundColor: 0x1099bb});
 document.body.appendChild(app.view);//文档对象模型
 
 var basicText = new PIXI.Text('2048',{
-    fontSize:70
+    fontSize:150
 });
 
 basicText.anchor.set(0.5);
-basicText.x = app.renderer.width/3;//230;
-basicText.y = app.renderer.height/8;//90;
+basicText.x = app.renderer.width/2;//230;
+basicText.y = app.renderer.height/4;//90;
 
 app.stage.addChild(basicText);
 
@@ -23,7 +23,7 @@ for(var i=0;i<4;i++){
     for(var j=0;j<4;j++){
         var graphics=new PIXI.Graphics();
         graphics.beginFill(0xFFCD72, 1);
-        graphics.drawRect(app.renderer.width/15+102*j, app.renderer.height/20*5+102*i, 100, 100);
+        graphics.drawRect(app.renderer.width/8+160*j, app.renderer.height/8*3+160*i,156, 156);
         app.stage.addChild(graphics);
     }
 }
